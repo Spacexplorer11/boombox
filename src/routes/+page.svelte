fef63ef90ce6b85229b3cb26bfd76e2dbc4b39c9
Akaalroop Singh <akaal@akaalroop.com>
2025-10-18 22:28:50 +0100

    Add some text and the boombox image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script>
+</script>
 
+<sveltekit:head>
+	<title>BoomBox - Home</title>
+	<meta name="description" content="BoomBox - In the most literal sense ever. This is based on the Kurzgesagt fandom!" />
+</sveltekit:head>
+<div class="@container flex flex-col">
+	<header class="@container/header text-center">
+<h1 class="text-4xl md:text-5xl">BoomBox</h1>
+<h3 class="text-2xl md:text-3xl">In the most literal sense ever</h3>
+<h3 class="text-2xl md:text-3xl">This is based on the Kurzgesagt fandom!</h3>
+	</header>
+	<main class="@container/main">
+		<div class="@container/boombox min-w-full">
+			<img src="/images/boombox.webp" alt="BoomBox" class="mx-auto my-4 w-full object-cover aspect-[16/9]" />
+		</div>
+	</main>
+</div>
