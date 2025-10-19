2f6bae2cd682989ec07a5225309eace49de98ad5
Akaalroop Singh <akaal@akaalroop.com>
2025-10-19 18:25:12 +0100

    Ran prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,24 @@
 
 <sveltekit:head>
 	<title>BoomBox - Home</title>
-	<meta name="description" content="BoomBox - In the most literal sense ever. This is based on the Kurzgesagt fandom!" />
+	<meta
+		name="description"
+		content="BoomBox - In the most literal sense ever. This is based on the Kurzgesagt fandom!"
+	/>
 </sveltekit:head>
 <div class="@container flex flex-col">
 	<header class="@container/header text-center text-red-950">
-<h1 class="text-4xl md:text-5xl">BoomBox</h1>
-<h3 class="text-2xl md:text-3xl">In the most literal sense ever</h3>
-<h3 class="text-2xl md:text-3xl">This is based on the Kurzgesagt fandom!</h3>
+		<h1 class="text-4xl md:text-5xl">BoomBox</h1>
+		<h3 class="text-2xl md:text-3xl">In the most literal sense ever</h3>
+		<h3 class="text-2xl md:text-3xl">This is based on the Kurzgesagt fandom!</h3>
 	</header>
 	<main class="@container/main">
 		<div class="@container/boombox min-w-full">
-			<img src="/images/boombox.webp" alt="BoomBox" class="mx-auto my-4 w-full object-cover aspect-[16/9]" />
+			<img
+				src="/images/boombox.webp"
+				alt="BoomBox"
+				class="mx-auto my-4 aspect-[16/9] w-full object-cover"
+			/>
 		</div>
 	</main>
 </div>
