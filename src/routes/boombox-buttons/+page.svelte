1917a6ce526ceb5b5c22706c112b4942fe0740ca
Akaalroop Singh <akaal@akaalroop.com>
2025-10-21 17:13:38 +0100

    Added dysfunctional placeholder buttons

--- src/routes/boombox-buttons/+page.svelte
+++ src/routes/boombox-buttons/+page.svelte
@@ -14,4 +14,10 @@
 		<h3 class="text-2xl md:text-3xl">A bunch of random buttons doing random things</h3>
 		<h3 class="text-2xl md:text-3xl">Caution! Be prepared</h3>
 	</header>
+	<main class="@container/main">
+		<div class="@container/side-by-side mt-10 flex flex-row">
+			<button class="mx-auto max-w-fit rounded-2xl bg-amber-500 p-4 text-5xl">Play</button>
+			<button class="mx-auto max-w-fit rounded-2xl bg-amber-500 p-4 text-5xl">Record</button>
+		</div>
+	</main>
 </div>
