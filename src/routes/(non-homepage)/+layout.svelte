71b3db1b23a51881c8cff0534ce5fa66e3226693
Akaalroop Singh <akaal@akaalroop.com>
2025-10-23 17:03:48 +0100

    Added @lucide/svelte and a nice home button!

---
+++ src/routes/(non-homepage)/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { House } from "@lucide/svelte";
+	import { goto } from "$app/navigation";
+
+	let { children } = $props();
+</script>
+
+<button
+	onclick={goto("/")}
+	aria-label="Go to Home Page"
+	title="Go to Home Page"
+	class="fixed top-4 left-4 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+>
+	<House />
+</button>
+{@render children?.()}
