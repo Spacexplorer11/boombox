bd6d77d4593b365a4a4643eb4edd79a93c58ade2
Akaalroop Singh <akaal@akaalroop.com>
2025-10-23 18:56:52 +0100

    Added back button to boombox-buttons subpages

---
+++ src/routes/(non-homepage)/boombox-buttons/(subpages)/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { ArrowLeft } from "@lucide/svelte";
+	import { goto } from "$app/navigation";
+
+	let { children } = $props();
+</script>
+
+<button
+	onclick={goto("/boombox-buttons")}
+	aria-label="Go to BoomBox Buttons Page"
+	title="Go to BoomBox Buttons Page"
+	class="fixed top-4 left-14 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+>
+	<ArrowLeft />
+</button>
+{@render children?.()}
