57e26bf1ab8eaac4b0bccc65f3a530b9d02ccc79
Akaalroop Singh <akaal@akaalroop.com>
2025-10-27 22:10:45 +0000

    Added a score system!

--- src/routes/(non-homepage)/boombox-buttons/(subpages)/record/+page.svelte
+++ src/routes/(non-homepage)/boombox-buttons/(subpages)/record/+page.svelte
@@ -6,6 +6,8 @@
 	let doneQuestions = [];
 	let questionJSON = $derived(questions[randomQuestion]);
 
+	let score = $state(0);
+
 	let questionsElement = "";
 	let optionsElement = "";
 	let finishedElement = "";
@@ -17,6 +19,7 @@
 	function questionSelected(optionIndex) {
 		return () => {
 			if (questionJSON.answer === optionIndex) {
+				score += 1;
 				if (optionIndex === 0) {
 					option1Element.style.backgroundColor = "green";
 				}
@@ -84,6 +87,9 @@
 		<h2 id="question" class="my-10 text-3xl text-red-950 md:text-4xl">{questionJSON.question}</h2>
 		<div hidden="hidden" id="finished">
 			<h2 class="my-10 text-3xl text-red-950 md:text-4xl">You finished all the questions!</h2>
+			<h3 class="my-10 text-2xl text-red-950 md:text-4xl">
+				You got {score}/{questions.length} correct!
+			</h3>
 			<h3 class="my-10 text-2xl text-red-950 md:text-4xl">Wanna go again?</h3>
 			<button
 				class="mx-auto my-2 max-w-fit rounded-2xl bg-purple-600
@@ -93,6 +99,7 @@
 					optionsElement.hidden = "";
 					finishedElement.hidden = "hidden";
 					doneQuestions = [];
+					score = 0;
 					do {
 						randomQuestion = Math.floor(Math.random() * questions.length);
 					} while (doneQuestions.includes(randomQuestion));
