8a62b553289768bd243185d730b29bf158f15d3c
Akaalroop Singh <akaal@akaalroop.com>
2025-10-27 21:24:17 +0000

    Added 2 questions and loading from JSON functionality. Also made it so that you can actually do the quiz, however it just uses alert() for correct/incorrect and finished atm

---
+++ src/lib/questions/index.js
@@ -0,0 +1,4 @@
+import q1 from "./1.json";
+import q2 from "./2.json";
+
+export default [q1, q2];
--- src/routes/(non-homepage)/boombox-buttons/(subpages)/record/+page.svelte
+++ src/routes/(non-homepage)/boombox-buttons/(subpages)/record/+page.svelte
@@ -1,9 +1,28 @@
 <script>
-	let Question = "Placeholder";
-	let Option1 = "Option 1";
-	let Option2 = "Option 2";
-	let Option3 = "Option 3";
-	let Option4 = "Option 4";
+	import questions from "$lib/questions";
+
+	let randomQuestion = $state(Math.floor(Math.random() * questions.length));
+	let doneQuestions = [];
+	let questionJSON = $derived(questions[randomQuestion]);
+
+	function questionSelected(optionIndex) {
+		return () => {
+			if (questionJSON.answer === optionIndex) {
+				alert("Correct!");
+			} else {
+				alert(`Incorrect! You can do the quiz again after finishing all questions.`);
+			}
+			doneQuestions.push(randomQuestion);
+			if (doneQuestions.length === questions.length) {
+				alert("You've completed all the questions!");
+				doneQuestions = [];
+			}
+			do {
+				randomQuestion = Math.floor(Math.random() * questions.length);
+			} while (doneQuestions.includes(randomQuestion));
+			questionJSON = questions[randomQuestion];
+		};
+	}
 </script>
 
 <sveltekit:head>
@@ -17,27 +36,31 @@
 		<h3 class="text-2xl md:text-3xl">Yeahhhh, good luck mate 🇬🇧</h3>
 	</header>
 	<main class="text-center">
-		<h2 class="my-10 text-3xl text-red-950 md:text-4xl">{Question}</h2>
+		<h2 class="my-10 text-3xl text-red-950 md:text-4xl">{questionJSON.question}</h2>
 		<div class="@container/options my-auto flex flex-col">
 			<button
 				class="mx-auto my-2 max-w-fit rounded-2xl bg-purple-600 p-4 text-2xl text-red-950 md:text-3xl"
+				onclick={questionSelected(0)}
 			>
-				{Option1}
+				{questionJSON.options[0]}
 			</button>
 			<button
 				class="mx-auto my-2 max-w-fit rounded-2xl bg-purple-600 p-4 text-2xl text-red-950 md:text-3xl"
+				onclick={questionSelected(1)}
 			>
-				{Option2}
+				{questionJSON.options[1]}
 			</button>
 			<button
 				class="mx-auto my-2 max-w-fit rounded-2xl bg-purple-600 p-4 text-2xl text-red-950 md:text-3xl"
+				onclick={questionSelected(2)}
 			>
-				{Option3}
+				{questionJSON.options[2]}
 			</button>
 			<button
 				class="mx-auto my-2 max-w-fit rounded-2xl bg-purple-600 p-4 text-2xl text-red-950 md:text-3xl"
+				onclick={questionSelected(3)}
 			>
-				{Option4}
+				{questionJSON.options[3]}
 			</button>
 		</div>
 	</main>
