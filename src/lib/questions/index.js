43e907101fd94c6f6a27034108d8ea6b7e7eec91
Akaalroop Singh <akaal@akaalroop.com>
2025-10-27 22:18:51 +0000

    Linked all the questions in index.js cuz I forgot to do so

--- src/lib/questions/index.js
+++ src/lib/questions/index.js
@@ -1,4 +1,7 @@
 import q1 from "./1.json";
 import q2 from "./2.json";
+import q3 from "./3.json";
+import q4 from "./4.json";
+import q5 from "./5.json";
 
-export default [q1, q2];
+export default [q1, q2, q3, q4, q5];
