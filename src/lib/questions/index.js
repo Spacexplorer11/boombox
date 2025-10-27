75d681399ce69109bc0cd92b2949cc297af63882
Akaalroop Singh <akaal@akaalroop.com>
2025-10-27 22:25:06 +0000

    Added + Linked! Q8

--- src/lib/questions/index.js
+++ src/lib/questions/index.js
@@ -5,5 +5,6 @@ import q4 from "./4.json";
 import q5 from "./5.json";
 import q6 from "./6.json";
 import q7 from "./7.json";
+import q8 from "./8.json";
 
-export default [q1, q2, q3, q4, q5, q6, q7];
+export default [q1, q2, q3, q4, q5, q6, q7, q8];
