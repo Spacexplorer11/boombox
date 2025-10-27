9d7dddd561ca6880bafb04a21e7d25f949838b16
Akaalroop Singh <akaal@akaalroop.com>
2025-10-27 22:22:52 +0000

    Added + Linked! Q6

--- src/lib/questions/index.js
+++ src/lib/questions/index.js
@@ -3,5 +3,6 @@ import q2 from "./2.json";
 import q3 from "./3.json";
 import q4 from "./4.json";
 import q5 from "./5.json";
+import q6 from "./6.json";
 
-export default [q1, q2, q3, q4, q5];
+export default [q1, q2, q3, q4, q5, q6];
