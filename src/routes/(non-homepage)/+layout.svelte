012782fdf10a908b24806c5dd3ea9d6f7b3d32e1
Akaalroop Singh <akaal@akaalroop.com>
2025-10-31 17:46:04 +0000

    Added GH sponsors and BMC buttons

--- src/routes/(non-homepage)/+layout.svelte
+++ src/routes/(non-homepage)/+layout.svelte
@@ -9,7 +9,7 @@
 	onclick={goto("/")}
 	aria-label="Go to Home Page"
 	title="Go to Home Page"
-	class="fixed top-4 left-2 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+	class="relative top-4 left-2 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
 >
 	<House />
 </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,20 @@
 	let { children } = $props();
 </script>
 
+<div id="sponsor-buttons" class="mt-2 flex flex-row items-center space-x-[70rem] lg:ml-auto">
+	<iframe
+		src="https://github.com/sponsors/Spacexplorer11/button"
+		title="Sponsor Spacexplorer11"
+		height="32"
+		width="114"
+		style="border: 0; border-radius: 6px;"
+	></iframe>
+	<a href="https://www.buymeacoffee.com/spacexplorer11" target="_blank" class="pointer"
+		><img
+			src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+			alt="Buy Me A Coffee"
+			style="height: 60px !important;width: 217px !important;"
+		/></a
+	>
+</div>
 {@render children?.()}
