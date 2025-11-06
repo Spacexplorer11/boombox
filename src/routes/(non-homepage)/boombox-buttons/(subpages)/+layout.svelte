bab6437a84827a1121ac9c54399bb994ae433c71
Akaalroop Singh <akaal@akaalroop.com>
2025-11-06 17:20:35 +0000

    Moved the back arrow so it doesnt overlap with gh sponsors button

--- src/routes/(non-homepage)/boombox-buttons/(subpages)/+layout.svelte
+++ src/routes/(non-homepage)/boombox-buttons/(subpages)/+layout.svelte
@@ -9,7 +9,7 @@
 	onclick={goto("/boombox-buttons")}
 	aria-label="Go to BoomBox Buttons Page"
 	title="Go to BoomBox Buttons Page"
-	class="fixed top-4 left-14 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+	class="relative top-4 left-4 rounded-full bg-white p-2 shadow-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:outline-none"
 >
 	<ArrowLeft />
 </button>
